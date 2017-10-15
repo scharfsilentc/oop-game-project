fc3d5d2c05a8aef610dc8b757cfb0e6f1b7afe12
Grace <grace.scharf@gmail.com>
2017-10-14 21:39:28 -0400

    work in progress

--- js/kittens.js
+++ js/kittens.js
@@ -9,13 +9,21 @@ var MAX_ENEMIES = 3;
 var PLAYER_WIDTH = 75;
 var PLAYER_HEIGHT = 54;
 
+var NEW_LIFE_WIDTH = 75;
+var NEW_LIFE_HEIGHT = 54;
+var MAX_NEW_LIVES = 2;
+
 // These two constants keep us from using "magic numbers" in our code
 var LEFT_ARROW_CODE = 37;
 var RIGHT_ARROW_CODE = 39;
+var UP_ARROW_CODE = 38;
+var DOWN_ARROW_CODE = 40;
 
 // These two constants allow us to DRY
 var MOVE_LEFT = 'left';
 var MOVE_RIGHT = 'right';
+var MOVE_UP = 'up';
+var MOVE_DOWN = 'down';
 
 // Preload game images
 var images = {};
@@ -30,27 +38,51 @@ var images = {};
 
 
 // This section is where you will be doing most of your coding
-class Enemy {
-    constructor(xPos) {
-        this.x = xPos;
-        this.y = -ENEMY_HEIGHT;
-        this.sprite = images['enemy.png'];
-
-        // Each enemy should have a different speed
-        this.speed = Math.random() / 2 + 0.25;
-    }
-
-    update(timeDiff) {
-        this.y = this.y + timeDiff * this.speed;
-    }
-
+class Entity {
     render(ctx) {
         ctx.drawImage(this.sprite, this.x, this.y);
     }
 }
 
-class Player {
+class FallingItem extends Entity {
     constructor() {
+        super();
+    }
+    update(timeDiff) {
+        this.y = this.y + timeDiff * this.speed;
+    }
+}
+class Enemy extends FallingItem{
+    constructor(xPos, score) {
+        super();
+        this.x = xPos;
+        this.y = -ENEMY_HEIGHT;
+        this.sprite = images['enemy.png'];
+        this.score = score;
+
+        // Each enemy should have a different speed
+        var excelerator = score > 2500 ? 2 : score > 3500 ? 3 : 1;
+        this.speed = (Math.random() / 3 + 0.2) * excelerator;
+    }
+
+}
+
+class NewLife extends FallingItem{
+    constructor(xPos) {
+        super();
+        this.x = xPos;
+        this.y = -NEW_LIFE_HEIGHT;
+        this.sprite = images['player.png'];
+       
+        //var excelerator = score > 2500 ? 2 : score > 3500 ? 3 : 1;
+        this.speed = (Math.random() / 2 + 0.25);
+
+    }
+}
+
+class Player extends Entity{
+    constructor() {
+        super();
         this.x = 2 * PLAYER_WIDTH;
         this.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
         this.sprite = images['player.png'];
@@ -63,12 +95,16 @@ class Player {
         }
         else if (direction === MOVE_RIGHT && this.x < GAME_WIDTH - PLAYER_WIDTH) {
             this.x = this.x + PLAYER_WIDTH;
+        } 
+        else if (direction === MOVE_UP && this.y > 0) {
+            this.y = this.y - PLAYER_HEIGHT;
+        } 
+        else if (direction === MOVE_DOWN && this.y < GAME_HEIGHT - PLAYER_HEIGHT) {
+            this.y = this.y + PLAYER_HEIGHT;
         }
     }
 
-    render(ctx) {
-        ctx.drawImage(this.sprite, this.x, this.y);
-    }
+    
 }
 
 
@@ -84,10 +120,12 @@ class Engine {
     constructor(element) {
         // Setup the player
         this.player = new Player();
-
         // Setup enemies, making sure there are always three
         this.setupEnemies();
-
+        //set lives
+        this.lives = 2;
+        //setup new lives
+       // this.setupNewLives();
         // Setup the <canvas> element where we will be drawing
         var canvas = document.createElement('canvas');
         canvas.width = GAME_WIDTH;
@@ -116,15 +154,51 @@ class Engine {
 
     // This method finds a random spot where there is no enemy, and puts one in there
     addEnemy() {
-        var enemySpots = GAME_WIDTH / ENEMY_WIDTH;
+        var enemySpots = GAME_WIDTH / ENEMY_WIDTH; //5
 
-        var enemySpot;
+        var enemySpot; //undefined
         // Keep looping until we find a free enemy spot at random
-        while (!enemySpot || this.enemies[enemySpot]) {
+        while (enemySpot === undefined || this.enemies[enemySpot]) {
             enemySpot = Math.floor(Math.random() * enemySpots);
         }
 
-        this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH);
+        this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH, this.score);
+        console.log('this is enemy spot',enemySpot*ENEMY_WIDTH);
+        
+    }
+
+    // setupNewLives() {
+    //     this.newLives = [];
+    //     while (newLives.length < MAX_NEW_LIVES) {
+    //         this.addNewLife();
+    //     }
+       // if (!this.newLives) {
+        //     this.newLives = [];
+        // }
+       
+
+        // while (this.newLives.length < MAX_NEW_LIVES) {
+        //     this.addNewLife();
+        // }
+    
+    // }
+
+    // addNewLife() {
+    //     var newLifeSpots = GAME_WIDTH / NEW_LIFE_WIDTH;
+    //     var newLifeSpot; //undefined
+        // // Keep looping until we find a free enemy spot at random
+        // while (newLifeSpot === undefined || this.newLives[newLifeSpot]) {
+        //     newLifeSpot = Math.floor(Math.random() * newLifeSpots);
+        // }
+
+    //     // this.newLifeSpots[newLifeSpot] = new NewLife(newLifeSpot * NEW_LIFE_WIDTH);
+    //     console.log('this is NEW LIFE spot',NEW_LIFE_WIDTH);
+
+    // }
+
+    lostLife() {
+        this.lives = this.lives - 1;
+        return this.lives;
     }
 
     // This method kicks off the game
@@ -140,8 +214,26 @@ class Engine {
             else if (e.keyCode === RIGHT_ARROW_CODE) {
                 this.player.move(MOVE_RIGHT);
             }
+            else if (e.keyCode === UP_ARROW_CODE) {
+                this.player.move(MOVE_UP);
+            }
+            else if (e.keyCode === DOWN_ARROW_CODE) {
+                this.player.move(MOVE_DOWN);
+            }
         });
 
+        this.gameLoop();
+        addNewLife();
+    }
+
+    restart() {
+        document.getElementById("controls").style.display = "none";
+        this.score = 0;
+        this.lastFrame = Date.now();
+        this.enemies = [];
+        this.player.x = 2 * PLAYER_WIDTH;
+        this.player.y = GAME_HEIGHT - PLAYER_HEIGHT - 10;
+        this.lives = 2;
         this.gameLoop();
     }
 
@@ -159,16 +251,17 @@ class Engine {
         // Check how long it's been since last frame
         var currentFrame = Date.now();
         var timeDiff = currentFrame - this.lastFrame;
-
         // Increase the score!
         this.score += timeDiff;
 
-        // Call update on all enemies
+        // Call update on all enemies & new lives
         this.enemies.forEach(enemy => enemy.update(timeDiff));
+        // this.newLives.forEach(newLife => newLife.update(timeDiff));
 
         // Draw everything!
         this.ctx.drawImage(images['stars.png'], 0, 0); // draw the star bg
         this.enemies.forEach(enemy => enemy.render(this.ctx)); // draw the enemies
+      // this.newLives.forEach(newLife => newLife.render(this.ctx)); // draw new lives
         this.player.render(this.ctx); // draw the player
 
         // Check if any enemies should die
@@ -178,13 +271,31 @@ class Engine {
             }
         });
         this.setupEnemies();
+       
+        // check if any new lives should die
+        // this.newLives.forEach((newLife, newLifeIdx) => {
+        //     if (newLife.y > GAME_HEIGHT) {
+        //         delete this.newLives[newLifeIdx];
+        //     }
+        // });
+        // this.setupNewLives();
 
         // Check if player is dead
         if (this.isPlayerDead()) {
             // If they are dead, then it's game over!
-            this.ctx.font = 'bold 30px Impact';
+           // this.ctx.textAlign = 'center'
+            this.ctx.font = "60px Verdana";
+            // Create gradient
+            var gradient = this.ctx.createLinearGradient(0,0,GAME_WIDTH,0);
+            gradient.addColorStop("0","magenta");
+            gradient.addColorStop("0.5","blue");
+            gradient.addColorStop("1.0","red");
+            //  Fill with gradient
+            this.ctx.fillStyle = gradient;
+            this.ctx.fillText('GAME OVER', 5, GAME_HEIGHT / 3);
+            this.ctx.font = 'bold 30px Calibri';
             this.ctx.fillStyle = '#ffffff';
-            this.ctx.fillText(this.score + ' GAME OVER', 5, 30);
+            this.ctx.fillText(this.score + '... not too shabby!', 50, 300)
         }
         else {
             // If player is not dead, then draw the score
@@ -199,15 +310,35 @@ class Engine {
     }
 
     isPlayerDead() {
-        // TODO: fix this function!
-        return false;
+        var checkForDead = (enemy) => {
+            if (this.lives < 1) {
+                document.getElementById("controls").style.display = "block";
+                return true
+            }
+            if(enemy.x === this.player.x && enemy.y + ENEMY_HEIGHT > this.player.y && enemy.y < this.player.y + PLAYER_HEIGHT)  {
+                this.lostLife();
+                this.enemies.forEach((enemy, enemyIdx) => {
+                    {delete this.enemies[enemyIdx];
+                    }
+                });
+                
+            }
+        }
+        return this.enemies.some(checkForDead)
+        
     }
 }
 
+
+var restartGame = function() {
+    gameEngine.restart();
+}
+document.getElementById("restart-button").onclick = restartGame;
 
 
 
 
 // This section will start the game
 var gameEngine = new Engine(document.getElementById('app'));
+
 gameEngine.start();
